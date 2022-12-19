6a557843d05f0b5cb1bfaf55d001aec71d97f50a
Junho-06 <k34920032@gmail.com>
2022-12-19 16:20:10 +0900

    이모지 오류 수정해야함

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import os
 import sys
 import urllib.request
 import json
+import emoji
+import asyncio
 
 import translation
 import TOKEN
@@ -22,8 +24,10 @@ async def on_ready():
 async def on_message(message):
     if message.content.startswith("pp 번역 "):
         
+        # 번역할 문장 변수 선언
         lan = str(message.content[6:])
         
+        # Papago OpenAPI 사용 문장
         client_id = TOKEN.CLIENT_ID
         client_secret = TOKEN.CLIENT_SECRET
         encQuery = urllib.parse.quote(lan)
@@ -34,11 +38,31 @@ async def on_message(message):
         request.add_header("X-Naver-Client-Secret",client_secret)
         response = urllib.request.urlopen(request, data=data.encode("utf-8"))
         rescode = response.getcode()
+
         if(rescode==200):
             response_body = response.read()
             result = response_body.decode('utf-8')
-            answer = json.loads(result)
-            print(answer)
-            await translation.en_to_kr_translation(message)
+            lang = json.loads(result)
+
+            # 번역할 타겟 언어 선택
+            response = await message.channel.send("문장을 어떤 언어로 번역 하시겠습니까?")
+            await response.add_reaction("🇰🇷")
+            await response.add_reaction("🇺🇸")
+            await response.add_reaction("🇯🇵")
+            await response.add_reaction("🇨🇳")
+            await response.add_reaction("🇩🇪")
+
+            try:
+                def check(reaction, user):
+                    return str(reaction) in ['🇰🇷', '🇺🇸', '🇯🇵', '🇨🇳', '🇩🇪'] and \
+                    user == message.author and reaction.message.id == response.id
+                    
+                reaction, user = await app.wait_for('reaction_add', check=check, timeout=30.0)
+            except asyncio.TimeoutError:
+                await message.channel.send("너무 반응이 오래걸려요.")
+                await response.delete()
+            else:
+                await translation.translation(message, lang, reaction)
+                await response.delete()
 
 app.run(TOKEN.DISCORD_BOT_TOKEN)
--- translation.py
+++ translation.py
@@ -4,16 +4,47 @@ import sys
 import urllib.request
 import TOKEN
 import json
+import emoji
 
-async def kr_to_en_translation(message):
+
+def reaction_to_emoji(answer_reaction):
+    if emoji.demojize(answer_reaction) == emoji.emojize(':flag: United States:'):
+        return "en"
+    elif emoji.demojize(answer_reaction) == emoji.demojize('🇰🇷'):
+        return "ko"
+    elif emoji.demojize(answer_reaction) == emoji.demojize('🇯🇵'):
+        return "ja"
+    elif emoji.demojize(answer_reaction) == emoji.demojize('🇩🇪'):
+        return "de"
+    elif emoji.demojize(answer_reaction) == emoji.emojize(':flag: China:'):
+        return "zh-CN"
+    else:
+        return "error"
+
+# 들어온 메세지, 감지된 언어, 바꿀 언어가 함수의 인자로 들어옴
+async def translation(message, result, reaction):
+        # Papago OpenAPI 셋업
         client_id = TOKEN.CLIENT_ID
         client_secret = TOKEN.CLIENT_SECRET
 
+        # 번역할 문장 변수 선언
         lan = str(message.content[6:])
 
+        # 감지된 언어 코드 (ex: ko, en ..)
+        lang = str(result['langCode'])
+
         encText = urllib.parse.quote(lan)
-        data = "source=ko&target=en&text=" + encText
+        
+        print(lang, reaction)
+        print(emoji.demojize(reaction))
+
+        toCon = reaction_to_emoji(reaction)
+        print(toCon)
+
+        data = "source=" + lang + "&target=" + toCon + "&text=" + encText
+
         url = "https://openapi.naver.com/v1/papago/n2mt"
+
         request = urllib.request.Request(url)
         request.add_header("X-Naver-Client-Id",client_id)
         request.add_header("X-Naver-Client-Secret",client_secret)
@@ -21,27 +52,6 @@ async def kr_to_en_translation(message):
         rescode = response.getcode()
         if(rescode==200):
             response_body = response.read()
-            result = response_body.decode('utf-8')
-            answer = json.loads(result)
-            await message.channel.send(answer['message']['result']['translatedText'])
-
-
-async def en_to_kr_translation(message):
-        client_id = TOKEN.CLIENT_ID
-        client_secret = TOKEN.CLIENT_SECRET
-
-        lan = str(message.content[6:])
-
-        encText = urllib.parse.quote(lan)
-        data = "source=en&target=ko&text=" + encText
-        url = "https://openapi.naver.com/v1/papago/n2mt"
-        request = urllib.request.Request(url)
-        request.add_header("X-Naver-Client-Id",client_id)
-        request.add_header("X-Naver-Client-Secret",client_secret)
-        response = urllib.request.urlopen(request, data=data.encode("utf-8"))
-        rescode = response.getcode()
-        if(rescode==200):
-            response_body = response.read()
-            result = response_body.decode('utf-8')
-            answer = json.loads(result)
+            trans = response_body.decode('utf-8')
+            answer = json.loads(trans)
             await message.channel.send(answer['message']['result']['translatedText'])
